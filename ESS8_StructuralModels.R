322f86b188ef472aa53446bdb5229527e4fd0cbd
Meijun Yao <u0172378@kuleuven.be>
2025-06-18 17:57:57 +0200

    Add justification and codes for selecting 4-cluster solution

--- ESS8_StructuralModels.R
+++ ESS8_StructuralModels.R
@@ -54,6 +54,7 @@ Str_model<-'
 CCPolicySupport~SelfTran+Conser+SelfEnhan
 '
 
+##--------------------------------------------------------------------------------------------------------
 ##Model selection 
 #
 ##option C: CCPolSupport partial metric with support 2 as marker:
@@ -91,9 +92,17 @@ plot(BasicModel.PMetricCCPolSup.marker2.Selection$Overview$Clusters, BasicModel.
 lines(BasicModel.PMetricCCPolSup.marker2.Selection$Overview$Clusters, 
       BasicModel.PMetricCCPolSup.marker2.Selection$Overview$BIC_G, col="black", lwd=1)
 
-##suggest 3 clusters --> however, the 3-cluster solution put the extreme values at both sides together
+##suggest 3 clusters --> however, when comparing the 3-cluster solution and the 4-cluster solution with the below codes,
+#we notice that the 3-cluster solution put the extreme values at both sides together
+round(BasicModel.PMetricCCPolSup.marker2.Selection$Models$`3-cluster model`$posteriors[,1:3],digits = 4) #3-cluster membership
+round(BasicModel.PMetricCCPolSup.marker2.Selection$Models$`4-cluster model`$posteriors[,1:4],digits = 4) #4-cluster membership
+BasicModel.PMetricCCPolSup.marker2.Selection$Models$`3-cluster model`$param$beta_ks #3-cluster solution: regression coefficients for each cluster
+BasicModel.PMetricCCPolSup.marker2.Selection$Models$`4-cluster model`$param$beta_ks #4-cluster solution: regression coefficients for each cluster
 #
 ##Therefore, we go with 4-cluster to allow the countries with the opposite extreme values to be separated into two clusters
+
+###-----------------------------------------------------------------------------------------------------------------------------
+##Run 4-cluster solution to interpret the clusters
 CCPolicySupport.4clus.50S.MarkSup2.PM<-MMGSEM(dat=ESS8,
                                               S1 = list(NoOpen.HV.Metric.M2.Marker, CCPolSupport.PMetric.M1.MarkerSup2),
                                               S2 = Str_model,
