b861d9abe690dcd0e75c0c7dd87079d1a39fd5ee
Meijun Yao <u0172378@kuleuven.be>
2025-09-15 10:29:10 +0200

    using lavaan sam() for validation

--- Step-by-step Tutorial/Step 3 - Validation with SAM via MGSEM estimation.R
+++ Step-by-step Tutorial/Step 3 - Validation with SAM via MGSEM estimation.R
@@ -14,83 +14,46 @@ library(mmgsem)
 ############## SAM with MGSEM estimation to validate the clusters ##########################
 ############################################################################################
 
-##extract the loadings and residual variances from HV 
-EST_HV<-lavInspect(NoOpen.HV.Metric.Fit2.Marker, what = "est")
-lambda_HV_23cntry<-lapply(EST_HV, "[[", "lambda")
-theta_HV_23cntry<-lapply(EST_HV, "[[", "theta")
-#
-##extract the loadings and residual variances from climate policy support
-EST_CCPolSupport<-lavInspect(CCPolSupport.PMetric.Fit1.MarkerSup2, what = "est")
-lambda_CCPolSupport_23cntry<-lapply(EST_CCPolSupport, "[[","lambda")
-theta_CCPolSupport_23cntry<-lapply(EST_CCPolSupport, "[[","theta")
-#
-##initialize empty list to store the new lambda matrix, new theta matrix and the mapping matrix
-lambda_23cntry<-vector(mode = "list", length=length(unique(ESS8$country)))
-theta_23cntry<-vector(mode = "list", length=length(unique(ESS8$country)))
-Mmatrix<-vector(mode = "list", length = length(unique(ESS8$country)))
-
-for (g in 1:length(unique(ESS8$country))){
-  ##put lambda from two measurement blocks into into the same matrix for each group
-  lambda_23cntry[[g]]<-lav_matrix_bdiag(lambda_HV_23cntry[[g]], lambda_CCPolSupport_23cntry[[g]])
-  colnames(lambda_23cntry[[g]])<-c(colnames(lambda_HV_23cntry[[g]]), colnames(lambda_CCPolSupport_23cntry[[g]]))
-  rownames(lambda_23cntry[[g]])<-c(rownames(lambda_HV_23cntry[[g]]), rownames(lambda_CCPolSupport_23cntry[[g]]))
-  
-  ##put theta from two measurement blocks into the same matrix for each group
-  theta_23cntry[[g]]<-lav_matrix_bdiag(theta_HV_23cntry[[g]],theta_CCPolSupport_23cntry[[g]])
-  colnames(theta_23cntry[[g]])<-c(colnames(theta_HV_23cntry[[g]]), colnames(theta_CCPolSupport_23cntry[[g]]))
-  rownames(theta_23cntry[[g]])<-c(rownames(theta_HV_23cntry[[g]]), rownames(theta_CCPolSupport_23cntry[[g]]))
-  
-  ##compute the mapping matrix for each group
-  Mmatrix[[g]]<-solve(t(lambda_23cntry[[g]]) %*% solve(theta_23cntry[[g]]) %*% lambda_23cntry[[g]]) %*% t(lambda_23cntry[[g]]) %*% solve(theta_23cntry[[g]])
-}
-#
-##run an empty sem to just extract the imputed sample covariance matrix
-fake_model<-'
+####----------------------------------------------------------------------------------------------------
+#first, specify all the measurement models and structural model inside the same lavaan syntax
+HV.CliPolSup.FreeSAM.model<-'
+#measurement model for human values
 SelfTran=~ST4+ST1+ST2+ST3+ST5+SE3+C3+C4
 Conser=~C2+C1+C3+C4+C5+C6+SE4
 SelfEnhan=~SE2+SE1+SE3+SE4+C1
-
-##Add Error Term Correlation
 C5~~C6
 
+#measurement model for climate policy support
 CCPolicySupport=~support2+support1+support3
-'
 
-fake<-cfa(model = fake_model,
-          data = ESS8,
-          group = "country",
-          estimator="MLR",
-          missing="FIML",
-          do.fit=F)
-
-S<-fake@SampleStats@cov
-
-S<-lapply(S, function(x) {
-  colnames(x)<-rownames(x)<-colnames(fitted(fake)[[1]]$cov)
-  x
-})
-
-#
-##compute the step-1 factor covariance matrix for each group that will be used for the second step to estimate the regression coefficients of each group:
-Var_eta<-vector(mode = "list", length = length(unique(ESS8$country)))
-
-for (g in 1:length(unique(ESS8$country))) {
-  Var_eta[[g]]<-Mmatrix[[g]] %*% (S[[g]]-theta_23cntry[[g]]) %*% t(Mmatrix[[g]])
-}
-
-##In order to map the cluster solution, we also need to do a free SAM for all sorts of clustering solution:
-CCPolSupport_FREEsam_str_model<-'
+#structural model
 CCPolicySupport~SelfTran+Conser+SelfEnhan
 '
 
-CCPolSupport.FreeSAM.PM.Mark2<-cfa(model = CCPolSupport_FREEsam_str_model,
-                                   sample.cov = Var_eta,
-                                   sample.nobs = lavInspect(fake, "nobs"))
+
+#then use sam() to estimate the regression coefficient for each country
+CCPolSupport.lavaanFreeSAM.PM.Mark2<-sam(model = HV.CliPolSup.FreeSAM.model,
+                                   data = ESS8,
+                                   mm.list = list(c("SelfTran","Conser","SelfEnhan"),"CCPolicySupport"), ##to separate the measurement block
+                                   cmd = "sem",
+                                   group="country",
+                                   sam.method = "local", ##local SEM where we use factor covariance matrix from step 1 to estimate the structural model in step 2
+                                   group.equal="loadings", #metric invariance
+                                   group.partial=c("SelfEnhan=~SE3", "CCPolicySupport=~support3"), #non-invariant items from the results of Step 1 - Measurement Model
+                                   missing="FIML",  #handle missing values
+                                   bounds="wide" ##since CCPolicySupport measurement block needs the wide bounded estimation before
+)
+
+sink("C:/Users/U0172378/OneDrive - KU Leuven/Desktop/Meijun - PhD/R/MMGSEM_ESS_HumanValues-AntiImmigrant/Sink Output/BasicModel_lavaanSAM.txt")
+summary(CCPolSupport.lavaanFreeSAM.PM.Mark2, standardized=T)
+sink()
+####----------------------------------------------------------------------------------------------------
 
 
-
+####---------------------------------------------------------------------------------------------------
+#4 clusters
 ##plot the regression coefficients of each group and separate the cluster using color-coded
-FreeSAMparam<-parameterEstimates(CCPolSupport.FreeSAM.PM.Mark2)
+FreeSAMparam<-parameterEstimates(CCPolSupport.lavaanFreeSAM.PM.Mark2)
 FreeSAM_reg_param<-FreeSAMparam %>%
   filter(op=="~") %>%
   select(lhs, rhs, group, est) %>%
@@ -115,70 +78,6 @@ CCPolSupport_4clus_3D_PMMark2<-plot_ly(FreeSAM_reg_param, x= ~SelfTran, y= ~Cons
          ))
 
 
-htmlwidgets::saveWidget(CCPolSupport_4clus_3D_PMMark2, "BasicModel3D.html", selfcontained = T)
-
-
-########Alternative mapping for the paper:
-#
-#extract the group-specific parameter from free SAM:
-FreeSAMparam<-parameterEstimates(CCPolSupport.FreeSAM.PM.Mark2)
-FreeSAM_reg_param<-FreeSAMparam %>%
-  filter(op=="~") %>%
-  select(lhs, rhs, group, est) %>%
-  pivot_wider(names_from = rhs, values_from = est)
-#
-#merge with the cluster results:
-FreeSAM_reg_param_4clus<-merge(ClusterRes.4clus, FreeSAM_reg_param,
-                               by.x = "group",
-                               by.y = "group")
-#
-#for the dimension of self-transcendence value and conservation value:
-Convex_hull1<-FreeSAM_reg_param_4clus %>%
-  group_by(ClusMembership) %>%
-  slice(chull(SelfTran, Conser))
-
-ggplot(FreeSAM_reg_param_4clus, aes(x=SelfTran, y=Conser, color = factor(ClusMembership)))+
-  geom_point()+
-  geom_text(aes(label = country),vjust=-0.5, hjust=0.5)+
-  labs(title = "Self-transcendence and Conservation Dimension in 4 clusters",
-       color="cluster", fill="cluster")+
-  xlab("Self-Transcendence Value")+ylab("Conservation Value")+
-  theme_minimal()+
-  geom_polygon(data = Convex_hull1, aes(fill = factor(ClusMembership), color=factor(ClusMembership), group = ClusMembership),
-               alpha=0.1)+
-  guides(color=guide_legend(override.aes = list(fill=NA)))
-#
-#for the dimension of conservation and self-enhancement value:
-Convex_hull2<-FreeSAM_reg_param_4clus %>%
-  group_by(ClusMembership) %>%
-  slice(chull(Conser,SelfEnhan))
-
-ggplot(FreeSAM_reg_param_4clus, aes(x=Conser, y=SelfEnhan, color = factor(ClusMembership)))+
-  geom_point()+
-  geom_text(aes(label = country),vjust=-0.5, hjust=0.5)+
-  labs(title = "Self-transcendence and Conservation Dimension in 4 clusters",
-       color="cluster", fill="cluster")+
-  xlab("Conservation Value")+ylab("Self-Enhancement Value")+
-  theme_minimal()+
-  geom_polygon(data = Convex_hull2, aes(fill = factor(ClusMembership), color=factor(ClusMembership), group = ClusMembership),
-               alpha=0.1)+
-  guides(color=guide_legend(override.aes = list(fill=NA)))
-
-
-##################################################
-############## optional ##########################
-##################################################
-
-sam_CCPolSupport_4clus_PMMark2<-'
-CCPolicySupport~c(a3,a3,a3,a3,a3,a4,a3,a3,a3,a3,a2,a4,a4,a4,a3,a1,a3,a3,a4,a3,a4,a3,a4)*SelfTran+
-                c(b3,b3,b3,b3,b3,b4,b3,b3,b3,b3,b2,b4,b4,b4,b3,b1,b3,b3,b4,b3,b4,b3,b4)*Conser+
-                c(c3,c3,c3,c3,c3,c4,c3,c3,c3,c3,c2,c4,c4,c4,c3,c1,c3,c3,c4,c3,c4,c3,c4)*SelfEnhan
-'
-
-CCPolSupport.SAM.4clus.PMMark2<-cfa(model = sam_CCPolSupport_4clus_PMMark2,
-                                    sample.cov = Var_eta,
-                                    sample.nobs = lavInspect(fake, "nobs"))
-
-sink("C:/Users/U0172378/OneDrive - KU Leuven/Desktop/Meijun - PhD/Conference/EAM 2025/outputs for SEM lab - July 4/BasicModel_4clus_ConstrainEst.txt")
-summary(CCPolSupport.SAM.4clus.PMMark2, fit.measures=T, standardized=T)
-sink()
+htmlwidgets::saveWidget(CCPolSupport_4clus_3D_PMMark2, 
+                        file = "C:/Users/U0172378/OneDrive - KU Leuven/Desktop/Meijun - PhD/R/MMGSEM_ESS_HumanValues-AntiImmigrant/Sink Output/BasicModel3D.html", 
+                        selfcontained = T)
